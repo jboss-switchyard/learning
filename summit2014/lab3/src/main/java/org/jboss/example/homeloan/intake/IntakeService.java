9701600aba8034b32d8f50c7372990ddb53fc9dd
Kenneth Peeples <kpeeples@redhat.com>
2014-03-27 14:24:41 -0400

    Lab 3 update

--- summit2014/lab3/src/main/java/org/jboss/example/homeloan/intake/IntakeService.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.example.homeloan.intake;
-
-import org.jboss.example.homeloan.data.LoanApplication;
-
-public interface IntakeService {
-
-	void intake(LoanApplication application);
-}
--- summit2014/lab3/src/main/java/org/jboss/example/homeloan/status/StatusService.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.example.homeloan.status;
-
-import org.jboss.example.homeloan.data.Qualification;
-
-public interface StatusService {
-
-	Qualification status(String ssn);
-}
