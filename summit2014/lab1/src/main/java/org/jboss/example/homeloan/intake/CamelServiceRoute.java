392d5a27a04d201fa226ea938afd831bfaca27f9
Keith Babo <kbabo@redhat.com>
2014-03-28 10:20:21 -0400

    cleaned up CamelServiceRoute

--- summit2014/lab1/src/main/java/org/jboss/example/homeloan/intake/CamelServiceRoute.java
+++ summit2014/lab1/src/main/java/org/jboss/example/homeloan/intake/CamelServiceRoute.java
@@ -4,20 +4,16 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class CamelServiceRoute extends RouteBuilder {
 
-	public void configure() {
-		from("switchyard://IntakeService")
-			.setProperty("LoanApplication").simple("${body}")
-			.setBody().simple("${body.applicant.ssn}")
-			.to("switchyard://CustomerLookup")
+    public void configure() {
+        from("switchyard://IntakeService")
+            .setProperty("LoanApplication").simple("${body}")
+            .setBody().simple("${body.applicant.ssn}")
+            .to("switchyard://CustomerLookup")
             // BEGIN - additional routing logic
-            .filter(simple("${body} != null && ${body.size} > 0"))
-                .beanRef("Loan", "customerUpdate(${property.LoanApplication}, ${body})")
-                .setHeader("ExistingCustomer").constant(true)
-            .end()
-            .beanRef("Loan", "summary(${property.LoanApplication})")
+
             // END - additional routing logic
-			.setBody().property("LoanApplication")
-			.to("switchyard://PreQualificationService");
-	}
+            .setBody().property("LoanApplication")
+            .to("switchyard://PreQualificationService");
+    }
 
 }
