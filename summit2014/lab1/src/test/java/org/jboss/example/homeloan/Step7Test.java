231791f073af960d5a4bbda6f4a749c7c26e4212
Keith Babo <kbabo@redhat.com>
2014-04-01 15:09:14 -0400

    lab1 and tests complete

--- summit2014/lab1/src/test/java/org/jboss/example/homeloan/Step7Test.java
+++ summit2014/lab1/src/test/java/org/jboss/example/homeloan/Step7Test.java
@@ -1,14 +1,15 @@
 package org.jboss.example.homeloan;
 
+import org.jboss.example.homeloan.data.Qualification;
+import org.jboss.example.homeloan.prequal.LoanStatus;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.switchyard.component.test.mixins.cdi.CDIMixIn;
 import org.switchyard.component.test.mixins.http.HTTPMixIn;
-import org.switchyard.test.MockHandler;
+import org.switchyard.test.BeforeDeploy;
 import org.switchyard.test.SwitchYardRunner;
 import org.switchyard.test.SwitchYardTestCaseConfig;
-import org.switchyard.test.SwitchYardTestKit;
 
 @RunWith(SwitchYardRunner.class)
 @SwitchYardTestCaseConfig(
@@ -16,14 +17,28 @@ import org.switchyard.test.SwitchYardTestKit;
         mixins = { CDIMixIn.class, HTTPMixIn.class },
         exclude = "jms")
 public class Step7Test {
-    
-    private static final String URL = "http://localhost:18080/homeloan/IntakeService";
 
-    private HTTPMixIn restClient;
-    private SwitchYardTestKit testKit;
+    private static final String STATUS_MSG = "Approved from Test";
+    private static final String STATUS_URL = "http://localhost:18081/loanstatus/123";
+
+    private HTTPMixIn httpClient;
     
     @Test
-    public void sendREST() throws Exception {
+    public void checkStatus() throws Exception {
         
+        // Seed a qualification status
+        Qualification qual = new Qualification();
+        qual.setStatus(STATUS_MSG);
+        LoanStatus.updateStatus("123", qual);
+        
+        // Check status
+        String output = httpClient.sendString(STATUS_URL, "", HTTPMixIn.HTTP_GET);
+        Assert.assertTrue(output.contains(STATUS_MSG));
+    }
+    
+    @BeforeDeploy
+    public void setProperties() {
+        System.setProperty("org.switchyard.component.resteasy.standalone.port", "18081");
+        System.setProperty("org.switchyard.component.resteasy.standalone.path", "");
     }
 }
